9cc654238546845f8b3a9d82a936eaeda4097169
abhir <abhiramashok1062004@gmail.com>
2024-01-03 23:00:37 +0530

    Initial Commit

---
+++ tryouts/Calculator_Pro.java
@@ -0,0 +1,171 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Deque;
+import java.util.LinkedList;
+
+class MakuCalculatoR extends JFrame implements ActionListener
+{
+    JTextField jtxt1 = new JTextField();
+    JTextField jtxt2 = new JTextField();
+    JButton[] b;
+    String[] str={"7", "8","9","*","4","5","6","-","1","2","3","+","^","0",".","="};
+    Deque<String> Q =new LinkedList<>();
+    boolean flag=false;
+
+    public MakuCalculatoR()
+    {
+        this.setTitle("STANDARD CALC");
+        this.setSize(401,637);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(null);
+
+        jtxt1.setBounds(8,8,372,48);
+        jtxt1.setForeground(Color.blue);
+        jtxt1.setHorizontalAlignment(SwingConstants.RIGHT);
+        jtxt1.setFont(new Font("Ariel",Font.BOLD,20));
+        this.add(jtxt1);
+        jtxt2.setBounds(100,57,281,67);
+        jtxt2.setHorizontalAlignment(SwingConstants.RIGHT);
+        jtxt2.setFont(new Font("Ariel",Font.BOLD,30));
+        this.add(jtxt2);
+
+        b= new JButton[22];
+
+        b[0]=new JButton("CLEAR");
+        b[0].setBounds(8,58,90,65);
+        b[0].setFont(new Font("Ariel",Font.BOLD,16));
+        b[0].addActionListener(this);
+        this.add(b[0]);
+
+        b[1]= new JButton("<");
+        b[1].setBounds(8,128,90,90);
+        b[1].setFont(new Font("Ariel",Font.BOLD,31));
+        b[1].addActionListener(this);
+        this.add(b[1]);
+
+        b[2]= new JButton("(");
+        b[2].setBounds(102,128,44,90);
+        b[2].setFont(new Font("Ariel",Font.PLAIN,27));
+        b[2].addActionListener(this);
+        this.add(b[2]);
+
+        b[3]= new JButton(")");
+        b[3].setBounds(149,128,43,90);
+        b[3].setFont(new Font("Ariel",Font.PLAIN,27));
+        b[3].addActionListener(this);
+        this.add(b[3]);
+
+        b[4]= new JButton("%");
+        b[4].setBounds(196,128,90,90);
+        b[4].setFont(new Font("Ariel",Font.BOLD,30));
+        b[4].addActionListener(this);
+        this.add(b[4]);
+
+        b[5]= new JButton("/");
+        b[5].setBounds(290,128,90,90);
+        b[5].setFont(new Font("Ariel",Font.BOLD,30));
+        b[5].addActionListener(this);
+        this.add(b[5]);
+
+        int k=6,x=8,y=222,z=0;
+        for(int i=0;i<4;i++){
+            x=8;
+            for(int j=0;j<4;j++){
+                b[k]=new JButton(str[z++]);
+                b[k].setBounds(x,y,90,90);
+                b[k].setFont(new Font("Ariel",Font.BOLD,30));
+                b[k].addActionListener(this);
+                this.add(b[k++]);
+                x+=94;
+            }
+            y+=94;
+    }
+
+        this.setVisible(true);
+
+}
+    public void actionPerformed(ActionEvent e)
+    {
+        JButton b1= (JButton) e.getSource();
+        String txt= b1.getText();
+        System.out.print(txt);
+
+        try {
+            if (txt.equals("CLEAR")) {
+                jtxt1.setText(null);
+                jtxt2.setText(null);
+                System.out.println();
+                Q.removeAll(Q);
+            }
+            else if (txt.equals("<")) {
+                jtxt1.setText(jtxt1.getText().substring(0, jtxt1.getText().length() - 1));
+                jtxt2.setText(jtxt2.getText().substring(0, jtxt2.getText().length() - 1));
+            }
+            else if (!txt.equals("=")) {
+
+                if (jtxt1.getText().isEmpty() && txt.equals("-")) {
+                    // Helps to add 1st -ve element
+                    jtxt2.setText("-");
+                    jtxt1.setText("-");
+
+                } else if (jtxt1.getText().isEmpty() && isOperator(txt)) {
+                    // P`revents 1 element as operator
+                    return;
+                }
+                boolean flag2 = false;
+                if (isOperator(txt)) {
+                    String temp = jtxt1.getText().substring(jtxt1.getText().length() - 1);
+                    if (isOperator(temp)) {
+                        Q.removeLast();
+                        Q.add(txt);
+                        jtxt1.setText(jtxt1.getText().substring(0, jtxt1.getText().length() - 1) + txt);
+                        return;
+                    }
+                    if (!flag) {
+                        Q.add(jtxt2.getText());
+                    }
+                    Q.add(txt);
+                    flag2 = true;
+                }
+                //sets the 2 textFields
+                jtxt1.setText(jtxt1.getText() + txt);
+                jtxt2.setText(jtxt2.getText() + txt);
+
+                if (flag2) {
+                    jtxt2.setText(null);
+                    System.out.println("\nQUEUE " + Q);
+                }
+            }
+            else if (txt.equals("=")) {
+
+                flag = true;
+                if (jtxt2.getText().isEmpty()) {
+                    Q.removeLast();
+                    jtxt1.setText(jtxt1.getText().substring(0, jtxt1.getText().length() - 1));
+                    System.out.println("\nQUEUE " + Q);
+                    return;
+                }
+                Q.add(jtxt2.getText());
+                System.out.println("\nQUEUE " + Q);
+            }
+        }catch(Exception e1){
+            //e1.printStackTrace();
+        }
+    }
+    private boolean isOperator(String item){
+        if (item .equals("%") || item.equals("/") || item.equals("*") || item.equals("-")
+                || item.equals("+") || item.equals("^")) {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class Calculator_Pro
+{
+    public static void main(String[] args) {
+        new MakuCalculatoR();
+    }
+}
